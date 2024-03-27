6fd76208e8f3e83f765bf669e1f5c6b2c92c84b6
Leornadia <leornadiajose@gmail.com>
2024-03-27 01:02:25 -0700

    16-binary_tree_is_perfect.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,13 +2,13 @@
 
 /**
  * binary_tree_height - Measures the height of a binary tree
- * @tree: Pointer to the root node of the tree to measure the height
+ * @tree: Pointer to the root node of the tree
  *
- * Return: The height of the tree, or 0 if tree is NULL
+ * Return: The height of the tree, or 0 if the tree is NULL
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height = 0, right_height = 0;
+	size_t left_height, right_height;
 
 	if (tree == NULL)
 		return (0);
@@ -16,5 +16,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
-	return (left_height > right_height ? left_height + 1 : right_height + 1);
+	if (left_height > right_height)
+		return (left_height + 1);
+	else
+		return (right_height + 1);
 }
+
